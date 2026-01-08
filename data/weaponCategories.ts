7f352dbfaabcc75c17ede945a515103146cb30ac
Imagiro <thomas.brun.perso@gmail.com>
2026-01-08 16:47:16 +0100

    Ajout des compteurs récapitulatifs

--- data/weaponCategories.ts
+++ data/weaponCategories.ts
@@ -16,88 +16,77 @@ export const weaponCategories: Record<
     title: 'Lanceurs',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      (b.id.toString().startsWith('1000') || b.id.toString().startsWith('1001') || b.id.toString().startsWith('1003') || b.id.toString().startsWith('1004')) &&
-      b.description.toLowerCase().includes('4'),
+      (b.id.toString().startsWith('1000') || b.id.toString().startsWith('1001') || b.id.toString().startsWith('1003') || b.id.toString().startsWith('1004')),
   },
 
   rollers: {
     title: 'Rouleaux',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      b.id.toString().startsWith('1010') &&
-      b.description.toLowerCase().includes('4'),
+      b.id.toString().startsWith('1010'),
   },
 
   chargers: {
     title: 'Fusils',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      b.id.toString().startsWith('1020') &&
-      b.description.toLowerCase().includes('4'),
+      b.id.toString().startsWith('1020'),
   },
 
   sloshers: {
     title: 'Seaux',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      b.id.toString().startsWith('1030') &&
-      b.description.toLowerCase().includes('4'),
+      b.id.toString().startsWith('1030'),
   },
 
   splatlings: {
     title: 'Badigeonneurs',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      b.id.toString().startsWith('1040') &&
-      b.description.toLowerCase().includes('4'),
+      b.id.toString().startsWith('1040'),
   },
 
   dualies: {
     title: 'Doubles',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      b.id.toString().startsWith('1050') &&
-      b.description.toLowerCase().includes('4'),
+      b.id.toString().startsWith('1050'),
   },
 
   brellas: {
     title: 'Para-encre',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      b.id.toString().startsWith('1060') &&
-      b.description.toLowerCase().includes('4'),
+      b.id.toString().startsWith('1060'),
   },
 
   blasters: {
     title: 'Blasters',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      b.id.toString().startsWith('1002') &&
-      b.description.toLowerCase().includes('4'),
+      b.id.toString().startsWith('1002'),
   },
 
   brushes: {
     title: 'Épinceaux',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      b.id.toString().startsWith('1011') &&
-      b.description.toLowerCase().includes('4'),
+      b.id.toString().startsWith('1011'),
   },
 
   bows: {
     title: 'Arcs',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      b.id.toString().startsWith('1070') &&
-      b.description.toLowerCase().includes('4'),
+      b.id.toString().startsWith('1070'),
   },
 
   splatanas: {
     title: 'Éclatanas',
     filter: (b: Badge) =>
       b.category.includes('WeaponLevel') &&
-      b.id.toString().startsWith('1080') &&
-      b.description.toLowerCase().includes('4'),
+      b.id.toString().startsWith('1080'),
   },
 } as const;
 
