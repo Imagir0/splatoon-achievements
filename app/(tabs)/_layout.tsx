88b411c6745172f459b9a55f5ec44d0e2b3b8737
Imagiro <thomas.brun.perso@gmail.com>
2026-01-15 21:11:19 +0100

    hotfix merge

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -13,59 +13,61 @@ export default function TabsLayout() {
       <BannersProvider>
         <SalmonSkinsProvider>
           <WeaponsProvider>
-            <GearsProvider>
-              <Tabs
-                screenOptions={{
-                  // Supprime l'espace prévu pour les icônes
-                  //tabBarIconStyle: { display: 'none' },
-                  tabBarLabelStyle: {
-                    fontSize: 11,
-                  },
-                  tabBarActiveTintColor: '#e92626ff',
-                  tabBarInactiveTintColor: '#6b7280',
-                }}
-              >
-                <Tabs.Screen
-                  name="home"
-                  options={{
-                    title: 'Accueil',
-                    tabBarIcon: ({ color, size }) => (
-                      <Ionicons name="home-outline" size={size} color={color} />
-                    ),
+            <TableTurfProvider>
+              <GearsProvider>
+                <Tabs
+                  screenOptions={{
+                    // Supprime l'espace prévu pour les icônes
+                    //tabBarIconStyle: { display: 'none' },
+                    tabBarLabelStyle: {
+                      fontSize: 11,
+                    },
+                    tabBarActiveTintColor: '#e92626ff',
+                    tabBarInactiveTintColor: '#6b7280',
                   }}
-                />
+                >
+                  <Tabs.Screen
+                    name="home"
+                    options={{
+                      title: 'Accueil',
+                      tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="home-outline" size={size} color={color} />
+                      ),
+                    }}
+                  />
 
-                <Tabs.Screen
-                  name="collectibles"
-                  options={{
-                    title: 'Collectibles',
-                    tabBarIcon: ({ color, size }) => (
-                      <Ionicons name="grid-outline" size={size} color={color} />
-                    ),
-                  }}
-                />
+                  <Tabs.Screen
+                    name="collectibles"
+                    options={{
+                      title: 'Collectibles',
+                      tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="grid-outline" size={size} color={color} />
+                      ),
+                    }}
+                  />
 
-                <Tabs.Screen
-                  name="team"
-                  options={{
-                    title: 'Gestion équipe',
-                    tabBarIcon: ({ color, size }) => (
-                      <Ionicons name="people-outline" size={size} color={color} />
-                    ),
-                  }}
-                />
+                  <Tabs.Screen
+                    name="team"
+                    options={{
+                      title: 'Gestion équipe',
+                      tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="people-outline" size={size} color={color} />
+                      ),
+                    }}
+                  />
 
-                <Tabs.Screen
-                  name="stats"
-                  options={{
-                    title: 'Statistiques',
-                    tabBarIcon: ({ color, size }) => (
-                      <Ionicons name="stats-chart-outline" size={size} color={color} />
-                    ),
-                  }}
-                />
-              </Tabs>
-            </GearsProvider>
+                  <Tabs.Screen
+                    name="stats"
+                    options={{
+                      title: 'Statistiques',
+                      tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="stats-chart-outline" size={size} color={color} />
+                      ),
+                    }}
+                  />
+                </Tabs>
+              </GearsProvider>
+            </TableTurfProvider>
           </WeaponsProvider>
         </SalmonSkinsProvider>
       </BannersProvider>
