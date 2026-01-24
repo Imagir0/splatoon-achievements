6d3f1c1fe93fe75270583f98456b4dc787879626
Imagiro <thomas.brun.perso@gmail.com>
2026-01-24 12:08:33 +0100

    tableturf not lagging anymore !

--- app/(tabs)/collectibles/tableturf/list.tsx
+++ app/(tabs)/collectibles/tableturf/list.tsx
@@ -1,60 +1,26 @@
-import { COLORS } from '@/constants/colors';
+
 import { useTableTurf } from '@/contexts/TableTurfContext';
-import { tableTurfFilters } from '@/data/filters/tableTurfFilters';
 import { tableTurf } from '@/data/tableTurf';
+
 import { Stack } from 'expo-router';
 import React from 'react';
-import {
-  Alert,
-  Dimensions,
-  FlatList,
-  Image,
-  Platform,
-  Pressable,
-  StyleSheet,
-  ToastAndroid,
-  View,
-} from 'react-native';
+import { Alert, Dimensions, FlatList, Image, Platform, Pressable, StyleSheet, ToastAndroid, View } from 'react-native';
 
 export default function AllTableTurfScreen() {
   const { selectedTableTurf } = useTableTurf();
-
   const screenWidth = Dimensions.get('window').width;
   const numColumns = 10;
   const spacing = 2;
-  const tableTurfSize =
-    (screenWidth - spacing * (numColumns * 2)) / numColumns;
+  const tableTurfSize = (screenWidth - spacing * (numColumns * 2)) / numColumns;
 
   const handleTableTurfPress = (card: typeof tableTurf[0]) => {
     const message = card.name;
-
     if (Platform.OS === 'android') {
       ToastAndroid.show(message, ToastAndroid.SHORT);
     } else {
       Alert.alert('TableTurf', message);
     }
   };
-
-  const filterColors: Record<string, string> = {
-    season1: COLORS.shades.white,
-    season2: COLORS.shades.order,
-    season3: COLORS.shades.white,
-    season4: COLORS.shades.order,
-    season5: COLORS.shades.white,
-    season6: COLORS.shades.order,
-    season7: COLORS.shades.white,
-    season8: COLORS.shades.order,
-  };
-
-  function getTableTurfColor(card: (typeof tableTurf)[number]) {
-    const categoryKey = Object.keys(tableTurfFilters).find((key) =>
-      tableTurfFilters[key](card)
-    );
-
-    return categoryKey
-      ? filterColors[categoryKey] ?? COLORS.shades.order
-      : COLORS.shades.order;
-  }
 
   return (
     <View style={{ flex: 1, padding: spacing }}>
@@ -67,7 +33,6 @@ export default function AllTableTurfScreen() {
         numColumns={numColumns}
         renderItem={({ item }) => {
           const owned = !!selectedTableTurf[item.number];
-          const bgColor = getTableTurfColor(item);
 
           return (
             <Pressable
@@ -75,7 +40,7 @@ export default function AllTableTurfScreen() {
               style={[
                 styles.tableTurfWrapper,
                 {
-                  backgroundColor: bgColor,
+                  backgroundColor: 'white',
                   width: tableTurfSize,
                   height: tableTurfSize,
                   margin: spacing,
